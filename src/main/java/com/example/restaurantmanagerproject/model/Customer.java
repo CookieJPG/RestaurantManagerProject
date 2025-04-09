b567bfaff9d89c6c8a343a0e8760ef51e1e397aa
Juan <kylelovescookies047@gmail.com>
2025-04-09 14:08:11 -0400

    Updated Customer

--- src/main/java/com/example/restaurantmanagerproject/model/Customer.java
+++ src/main/java/com/example/restaurantmanagerproject/model/Customer.java
@@ -8,11 +8,21 @@ public abstract class Customer implements IRewardable {
     private String id;
     private String name;
     private Type type;
+    private String email;
+    private String phone;
+    private double loyaltyPoints;
 
     public Customer(String name, Type type) {
         this.name = name;
         this.type = type;
         setId();
+        this.loyaltyPoints = 0;
+    }
+
+    public Customer(String name, Type type, String email, String phone) {
+        this(name, type);
+        this.email = email;
+        this.phone = phone;
     }
 
     // Getters
@@ -52,4 +62,30 @@ public abstract class Customer implements IRewardable {
         }
     }
     public void setType(Type newType) { type = Objects.requireNonNullElse(newType, Type.First); }
+
+    // TODO: Verify that the email has an @something.smth
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    //TODO: Verify that the phone number is the correct length
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public double getLoyaltyPoints() {
+        return loyaltyPoints;
+    }
+    public void addLoyaltyPoints(double loyaltyPoints) {
+        this.loyaltyPoints += loyaltyPoints;
+    }
+    public void removeLoyaltyPoints(double loyaltyPoints) {
+        this.loyaltyPoints -= loyaltyPoints;
+    }
 }
